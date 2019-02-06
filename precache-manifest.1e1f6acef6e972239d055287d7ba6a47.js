33bc477b1ba375ad96b9e7577ce1346238d0ce98
Evgenii Mishalov <forwow495800@gmail.com>
2019-02-06 14:24:44 +0500

    added build

---
+++ precache-manifest.1e1f6acef6e972239d055287d7ba6a47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "12523ddbb16244b47860",
+    "url": "/static/js/main.12523ddb.chunk.js"
+  },
+  {
+    "revision": "479bbc09a470188a51c2",
+    "url": "/static/js/1.479bbc09.chunk.js"
+  },
+  {
+    "revision": "12523ddbb16244b47860",
+    "url": "/static/css/main.96f1e209.chunk.css"
+  },
+  {
+    "revision": "479bbc09a470188a51c2",
+    "url": "/static/css/1.296541e3.chunk.css"
+  },
+  {
+    "revision": "0fe1aa22a1f2fd38f562a6ec9d3f4dcc",
+    "url": "/index.html"
+  }
+];
