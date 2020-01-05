6faf72fbe09f592c1c1e1e1f39f8c4838f0bc0ae
Evgenii <mie@crtweb.ru>
2020-01-05 22:48:20 +0500

    refactoring

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,15 +1,11 @@
 import React, { Fragment } from "react";
-// import fetch from "isomorphic-fetch";
-import { Dropbox } from "dropbox";
 import PlaylistContainer from "./playlist/PlaylistContainer";
-import Authentication from "./auth/ Authentication";
+import Authentication from "./auth/Authentication";
 import { inject, observer } from "mobx-react";
 import { Store } from "../store/Store";
 import { Spin } from "antd";
 import PlayerContainer from "./player/PlayerContainer";
-import { isMobile } from "react-device-detect";
 import TopBar from "./topBar/TopBar";
-import { findGetParameter } from "../helpers/findGetParameter";
 
 interface IContainer {
   store?: Store;
@@ -18,17 +14,6 @@ interface IContainer {
 @inject("store")
 @observer
 class Container extends React.Component<IContainer> {
-  async componentDidMount() {
-    const { props } = this;
-    const { AuthStore, FilesStore } = this.props.store!;
-    // if (!findGetParameter("hash")) {
-    //   if (!AuthStore.OAuthPassed) {
-    //     props.history.push("/auth");
-    //   } else {
-    //     props.history.push("/");
-    //   }
-  }
-
   fetchFiles = () => {
     const { FilesStore } = this.props.store!;
     FilesStore.fetchFiles();
@@ -57,7 +42,6 @@ class Container extends React.Component<IContainer> {
               files={FilesStore.files}
               fetchFiles={this.fetchFiles}
               playFile={this.playFile}
-              // playNow={FilesStore.playNow}
             />
           </div>
         </Fragment>
